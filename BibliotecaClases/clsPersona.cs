e5666db0e7ebd0f09a922577cc1dd595f77f451c
Mdmgzz <mdominguezg45@gmail.com>
2023-09-28 13:55:44 +0200

    Agregar archivos de proyecto.

---
+++ BibliotecaClases/clsPersona.cs
@@ -0,0 +1,49 @@
+﻿namespace BibliotecaClases
+{
+    public class clsPersona
+    {
+        #region atributos
+        private String nombre;
+        private String apellidos;
+        #endregion
+
+        #region constructores
+        #endregion
+
+        public clsPersona()
+        {
+            nombre = "";
+            apellidos = "";
+        }
+        public clsPersona(string nombre, string apellidos)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+        }
+
+        #region propiedades
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public String Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+       
+
+        public String Direccion { get; set; }
+        public string NombreCompleto
+        {
+            get { return $"El nombre completo es : {nombre} {apellidos}"; }
+                
+        }
+        #endregion
+        #region funciones y metodos
+        4
+
+        #endregion
+    }
+}
